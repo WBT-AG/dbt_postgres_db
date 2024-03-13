b2f067c5e47a5d7daa2d9775df1a32e0e821b8fd
WBT-AG <alex.glencross@welbilt.com>
2024-03-13 13:44:18 +0000

    updated stg_asset

--- models/staging/salesforce/stg_salesforce__assets.sql
+++ models/staging/salesforce/stg_salesforce__assets.sql
@@ -6,14 +6,16 @@ final AS (
     SELECT 
     a.status,
     a.distribution_channel__c as distribution_channel,
-    a.series__c as series__c,
+    a.series__c as series,
     --a.asset_item_sku__c as sku,
     a.asset_description__c as description,
     a.asset_serial_number__c as serial_number,
-    a.asset_item_number__c as model_number,
+    UPPER(a.asset_item_number__c) as model_number,
     a.original_install_date__c as original_install_date,
-    a.gpl_description__c as gpl_description,
-    a."Id" as asset_id
+    UPPER(a.gpl_description__c) as gpl_description,
+    a."Id" as asset_id,
+    a.original_install_date__c as install_date,
+    a.sold_to_ship_country__c as sold_to_country_code
     FROM assets a
     WHERE a.brandtext__c = 'Merrychef' OR a.brandtext__c = 'MERRYCHEF'
 )
